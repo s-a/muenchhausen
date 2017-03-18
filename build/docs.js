04799f3b0ad1b998f341eaa4781ac705dd912941
Stephan Ahlf <stephan.ahlf@googlemail.com>
2017-03-18 09:17:25 +0100

    awesome

--- build/docs.js
+++ build/docs.js
@@ -1,12 +1,27 @@
+"use strict";
+
+
 fs = require("fs");
 path = require("path");
 
 
 var glob = require("glob")
 var options = {};
-var all = [];
+var all = []; 
 // options is optional
-var processFile = function processFile(file) {
+var xfiles;
+var processFiles = function processFiles(done) {
+	if(xfiles.length === 0){ 
+		done(); 
+	} else {
+		processFile(xfiles[0], function() {
+			xfiles.shift();
+			processFiles(done);
+		});
+	}
+};
+
+var processFile = function processFile(file, done) {
 	var dir = path.dirname(file)
 	var segments = dir.replace(/\\/g, "/").split("/");
 	segments.shift()
@@ -19,35 +34,32 @@ var processFile = function processFile(file) {
 	console.log(file, output)
 	var templateDir = "./node_modules/jsdox/templates/";
 	var jsdox = require("jsdox");
-	all.push(output);
 	jsdox.generateForDir(file, path.join(output, ".."), templateDir, function(str){
-		fs.rename('./docs/index.md', output, function(err) {
-		    if ( err ) console.error('ERROR: ' + err);
+		fs.rename("./docs/index.md", output, function(err) {
+			all.push(output);
+			if ( err ) console.error("ERROR: " + err);
+			done();
 		});
 	}); 
 };
 
 glob("./lib/**/*.js", options, function (er, files) {
-	for (var i = 0; i < files.length; i++) {
-		var file = files[i];
-		processFile(file)
-	}
+ 	xfiles = files;
+	processFiles(function(){
+		var md = "# muenchhausen API\n\n";
 
-	var md = "# muenchhausen API\n\n";
+		for (var i = 0; i < all.length; i++) {
+			var bn = path.basename(all[i], ".md");
+			all[i] = "[" + bn + "](" + bn + ".md)";
+		}
+		
+		md += "- " + all.sort(function(a, b) {
+	    	return a < b;
+		}).join("\n- ")
+		console.log("index.md written")
+		fs.writeFileSync("docs/index.md", md);
+	 
+	});
 
-	for (var i = 0; i < all.length; i++) {
-		var bn = path.basename(all[i], ".md");
-		all[i] = "[" + bn + "](" + bn + ".md)";
-	}
-	
-	md += "- " + all.sort(function(a, b) {
-    	return a < b;
-	}).join("\n- ")
-	console.log(md)
-	fs.writeFileSync("index.md", md);
-		fs.rename('index.md', './docs/index.md', function(err) {
-		    if ( err ) console.error('ERROR: ' + err);
-	    console.log("index file was saved!");
-		});
 
 })
--- docs/Muenchhausen.fake.date.md
+++ docs/Muenchhausen.fake.date.md
@@ -1,3 +1,32 @@
+# Global
+
+
+
+
+
+* * *
+
+### now(randomizeSettings) 
+
+Current date.
+
+**Parameters**
+
+**randomizeSettings**: `Object`, Example: `"today is: {{{date.now}}}"`
+
+
+
+### random(randomizeSettings) 
+
+Random date based on configuration options.
+
+**Parameters**
+
+**randomizeSettings**: `Object`, Example: `"a random date: {{{date.random 'suffix:.ad,min:20770901'}}}\trandom: {{{date.random}}}"`
+
+
+
+
 * * *
 
 
