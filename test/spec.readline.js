7eea019b512023e9810d22d9a17f29fc65fce4c8
s-a <stephan.ahlf@gmail.com>
2017-03-20 11:11:38 +0100

    skip large file test

--- test/spec.readline.js
+++ test/spec.readline.js
@@ -3,7 +3,7 @@
 var assert = require("assert");
 var rndline = require("../lib/random-line");
 
-describe("large files", function () {
+describe.skip("large files", function () {
   it("should read random line from large file fast!", function (done) {
     rndline("./lib/random-line/access_log", function (line) {
       assert(true, line !== null && line !== undefined);
