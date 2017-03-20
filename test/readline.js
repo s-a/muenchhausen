4a0eddc9eddf1d6c87f3da710276df1bcd11dff7
s-a <stephan.ahlf@gmail.com>
2017-03-20 08:32:12 +0100

    relax timeout

--- test/readline.js
+++ test/readline.js
@@ -4,7 +4,6 @@ var assert = require("assert");
 var rndline = require("../lib/random-line");
 
 describe("large files", function () {
-  this.timeout(100);
   it("should read random line from large file fast!", function (done) {
     rndline("./lib/random-line/access_log", function (line) {
       assert(true, line !== null && line !== undefined);
