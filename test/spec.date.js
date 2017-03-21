8796e4551a7c812a042c2c3e30ca5c6d7a43dae1
s-a <stephan.ahlf@gmail.com>
2017-03-21 13:04:24 +0100

    update date unit tests

--- test/spec.date.js
+++ test/spec.date.js
@@ -5,32 +5,32 @@ var Muenchhausen = require("../lib");
 
 describe("muenchhausen weekday name", function () {
   it("should render a weeday in english!", function () {
-    var muenchhausen = new Muenchhausen("en-GB");
+    var a = new Muenchhausen("en-GB");
     var merchandiseTVSpotText = "$(date.weekday)";
-    var news = muenchhausen.render(merchandiseTVSpotText);
+    var news = a.render(merchandiseTVSpotText);
     news.should.be.equalOneOf("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");  
   }); 
 
   it("should render a weekday in german!", function () {
-    var muenchhausen = new Muenchhausen("de-DE");
+    var b = new Muenchhausen("de-DE");
     var merchandiseTVSpotText = "$(date.weekday)";
-    var news = muenchhausen.render(merchandiseTVSpotText);
+    var news = b.render(merchandiseTVSpotText);
     news.should.be.equalOneOf("Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag");  
   }); 
 });
 
 describe("muenchhausen month name", function () {
   it("should render a month name in english!", function () {
-    var muenchhausen = new Muenchhausen("en-GB");
+    var c = new Muenchhausen("en-GB");
     var merchandiseTVSpotText = "$(date.month)";
-    var news = muenchhausen.render(merchandiseTVSpotText);
+    var news = c.render(merchandiseTVSpotText);
     news.should.be.equalOneOf("January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December");  
   }); 
 
   it("should render a month name in german!", function () {
-    var muenchhausen = new Muenchhausen("de-DE");
+    var d = new Muenchhausen("de-DE");
     var merchandiseTVSpotText = "$(date.month)";
-    var news = muenchhausen.render(merchandiseTVSpotText);
+    var news = d.render(merchandiseTVSpotText);
     news.should.be.equalOneOf("Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember");  
   }); 
 });
